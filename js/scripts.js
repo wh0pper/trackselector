98a496aa5ebb6d2ed7a9a471e04f92d9c21c9317
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-01-12 09:41:54 -0800

    complete form layout and start adding javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+
+    var name = $("input#name").val();
+
+    console.log(name);
+  });
 });
