831e89767434283f1e5a4ba9151577ddee0b6442
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-01-12 10:31:40 -0800

    start writing backend logic using branching

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,42 @@ $(document).ready(function() {
     event.preventDefault();
 
     var name = $("input#name").val();
+    var age = $("input#age").val();
+    var environment = $("select#environment").val();
+    var platform = $("select#platform").val();
+    var skillset = $("input:radio[name=skillset]:checked").val();
+    var result;
 
-    console.log(name);
+    $(".track").hide();
+
+    if (environment === "enterprise") {
+      if (platform === "mobile") {
+        $("#java").show();
+      } else if (platform === "internal" || platform === "embedded") {
+        $("#csharp").show();
+      }
+    }
+
+    if (environment === "startup" || environment === "agency") {
+      if (platform === "web") {
+        $("#ruby").show();
+      } else if (platform === "mobile") {
+        $("#java").show();
+      } else if (skillset === "creative") {
+        $("#design").show();
+      }
+    }
+
+    if (environment === "freelance") {
+      if (platform === "web") {
+        $("#ruby").show();
+      } else if (platform === "mobile") {
+        $("#java").show();
+      } else {
+        //suggest more research
+      }
+    }
+
+
   });
 });
