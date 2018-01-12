3ef20b11752952762ab80e190f198224f848601e
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-01-12 15:43:02 -0800

    update if statements on master branch

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,80 @@
 $(document).ready(function() {
   $("form#survey1").submit(function(event) {
     event.preventDefault();
-
     var name = $("input#name").val();
     var age = $("input#age").val();
     var environment = $("select#environment").val();
     var platform = $("select#platform").val();
     var skillset = $("input:radio[name=skillset]:checked").val();
-
-
     $(".track").hide();
-
     if (name) {
       $("span#name").text(", " + name);
     }
 
-    $("#message").show();
-
+    $("#message").slideDown();
     if (age >= 40) {
-      $("#age-message").show();
+      $("#age-message").slideDown();
     } else {
-      $("#age-message").hide();
+      $("#age-message").slideDown();
     }
+
+    if (platform === "web") {
+      if (environment === "freelance" || environment === "agency") {
+        $("#ruby").slideDown();
+        $("#php").slideDown();
+      } else if (environment === "startup") {
+        $("#ruby").slideDown();
+      } else {
+        $("#research").slideDown();
+      }
+    } else if (platform === "mobile") {
+      if (skillset === "creative"){
+        $("#design").slideDown();
+      } else {
+        $("#java").slideDown();
+      }
+    } else if (platform === "internal") {
+      if (environment === "enterprise") {
+        $("#csharp").slideDown();
+      } else {
+        $("#research").slideDown();
+      }
+    } else if (platform === "embedded") {
+        if (environment === "freelance" || environment === "agency") {
+          $("#research").slideDown();
+        } else {
+          $("#csharp").slideDown();
+        }
+    }
+
 
 
 // logic tree starting at 'desired work environment'
-    if (environment === "enterprise") {
-      if (platform === "mobile") {
-        $("#java").show();
-      } else if (platform === "internal" || platform === "embedded") {
-        $("#csharp").show();
-      }
-    } else if (environment === "startup" || environment === "agency") {
-      if (platform === "web") {
-        $("#ruby").show();
-      } else if (platform === "mobile") {
-        $("#java").show();
-      } else if (skillset === "creative") {
-        $("#design").show();
-      }
-    } else if (environment === "freelance") {
-      if (platform === "web") {
-        $("#ruby").show();
-      } else if (platform === "mobile") {
-        $("#java").show();
-      } else if (skillset === "creative") {
-        $("#design").show();
-      }
-    } else {
-      $("#research").show();
-    }
+    // if (environment === "enterprise") {
+    //   if (platform === "mobile") {
+    //     $("#java").show();
+    //   } else if (platform === "internal" || platform === "embedded") {
+    //     $("#csharp").show();
+    //   }
+    // } else if (environment === "startup" || environment === "agency") {
+    //   if (platform === "web") {
+    //     $("#ruby").show();
+    //   } else if (platform === "mobile") {
+    //     $("#java").show();
+    //   } else if (skillset === "creative") {
+    //     $("#design").show();
+    //   }
+    // } else if (environment === "freelance") {
+    //   if (platform === "web") {
+    //     $("#ruby").show();
+    //   } else if (platform === "mobile") {
+    //     $("#java").show();
+    //   } else if (skillset === "creative") {
+    //     $("#design").show();
+    //   }
+    // } else {
+    //   $("#research").show();
+    // }
 
     $("#resource-survey").slideDown();
   });
